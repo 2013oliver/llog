a5e0775f3209a7d8613fd94a5b0402a2cbed3124
2013oliver <90613804+2013oliver@users.noreply.github.com>
2023-07-22 15:12:40 +0800

    添加功能:添加条目

--- learning_logs/forms.py
+++ learning_logs/forms.py
@@ -1,11 +1,18 @@
 from django import forms;
 
-from .models import Topic;
+from .models import Entry, Topic;
 
 class TopicForm(forms.ModelForm):
     class Meta:
         model = Topic;
         fields = ['text'];
         labels = {'text':''};
+
+class EntryForm(forms.ModelForm):
+    class Meta:
+        model = Entry;
+        fields = ['text'];
+        lables = {'ttext':''};
+        widgets = {'text':forms.Textarea(attrs = {'cols':80})};
 
 
